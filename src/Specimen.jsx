37e296ecdc833debfeb9e189cf8ca3a4503cbb13
Louie Camacho <louiecamacho@gmail.com>
2026-02-05 16:16:18 -0600

    add glow to specimen

--- src/Specimen.jsx
+++ src/Specimen.jsx
@@ -1,3 +1,7 @@
+import {useTheme} from "./ThemeContext";
+
 export default function Specimen({ glow, specimen }) {
-  return <li className="specimen">{specimen}</li>;
+  const {isDark} = useTheme();
+
+  return <li className={"specimen " + (glow && isDark ? "glow" : "")}>{specimen}</li>;
 }
